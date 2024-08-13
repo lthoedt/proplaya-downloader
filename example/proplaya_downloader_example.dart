2a4a2fa8dd27ab089b79050d49acdfa03c3e5a8c
lthoedt <lthoedt@gmail.com>
2024-08-13 16:50:12 +0200

    initial package

---
+++ example/proplaya_downloader_example.dart
@@ -0,0 +1,6 @@
+import 'package:proplaya_downloader/proplaya_downloader.dart';
+
+void main() {
+  var awesome = Awesome();
+  print('awesome: ${awesome.isAwesome}');
+}
---
+++ test/proplaya_downloader_test.dart
@@ -0,0 +1,16 @@
+import 'package:proplaya_downloader/proplaya_downloader.dart';
+import 'package:test/test.dart';
+
+void main() {
+  group('A group of tests', () {
+    final awesome = Awesome();
+
+    setUp(() {
+      // Additional setup goes here.
+    });
+
+    test('First Test', () {
+      expect(awesome.isAwesome, isTrue);
+    });
+  });
+}
